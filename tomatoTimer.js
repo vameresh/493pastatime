15c0239eaa3222c60365a6161daedba091407446
vameresh <vameresh@umich.edu>
2020-11-18 01:21:51 -0500

    timer setup

--- tomatoTimer.js
+++ tomatoTimer.js
@@ -1,40 +1,103 @@
 let timerSet = {
     template:`   
     <div class = "timer-input">
-        <input type=text placeholder=0 v-model="hours" @focus = "redirectToSeconds" @change ="updateHours"></input>
+        <input type=text placeholder="00" v-model="hours" @focus = "focus" ref = "hours"></input>
         <h4>H</h4>
-        <input type=text placeholder=0 v-model="minutes" @focus = "redirectToSeconds" @change ="updateMinutes"></input>
+        <input type=text placeholder="25" v-model="minutes" @focus = "focus" ref = "minutes"></input>
         <h4>M</h4>
-        <input type=text placeholder=0 v-model="seconds" @change ="updateSeconds" ref = "seconds"></input>
+        <input type=text placeholder="00" v-model="seconds"  @focus = "focus" ref = "seconds" id = "begin"></input>
         <h4>S</h4>
+        <input type=text placeholder="00" v-model = "time" @input="update" @change="setTime" ref = "input" id = "time-form" ></input>
     </div>
     `,
     data: function(){
         return{
-            hours: 0,
-            minutes: 0,
-            seconds: 0,
-            time: 0
+            hours: null,
+            minutes: null,
+            seconds: null,
+            time: "",
+            cursor: null
         }
     },
     methods: {
-        updateHours(){
-            console.log("on change hour")
-            
-        },
-        updateMinutes(){
-            console.log("on change minutes")
-            
-        },
-        updateSeconds(){
-            console.log("on change seconds")
-            
-        },
-        redirectToSeconds(){
-            console.log("focused")
-            if(this.hours == 0 && this.minutes == 0 && this.seconds == 0){
-                this.$refs.seconds.focus();
+        update(){
+
+            if(!this.time.match(/[0-9]$/)){;
+                this.time = this.time.slice(0, this.time.length-1);
             }
+
+            if(this.time.length > 6){
+                this.time = this.time.slice(1);
+            }
+            if(this.time.length == 6){
+                this.hours = this.time.slice(0, 2);
+                this.minutes = this.time.slice(2, 4);
+                this.seconds = this.time.slice(4, 6);
+            }
+            if(this.time.length == 5){
+                this.hours = this.time.slice(0, 1);
+                this.hours = "0" + this.hours;
+                this.minutes = this.time.slice(1, 3);
+                this.seconds = this.time.slice(3, 5);
+            }
+            if(this.time.length == 4){
+                this.hours = null;
+                this.minutes = this.time.slice(0, 2);
+                this.seconds = this.time.slice(2, 4);
+            }
+            if(this.time.length == 3){
+                this.hours = null;
+                this.minutes = this.time.slice(0, 1);
+                this.minutes = "0" + this.minutes;
+                this.seconds = this.time.slice(1, 3);
+            }
+            if(this.time.length == 2){
+                this.hours = null;
+                this.minutes = null;
+                this.seconds = this.time;
+
+            }
+            if(this.time.length == 1){
+                this.hours = null;
+                this.minutes = null;
+                this.seconds = this.time;
+                this.seconds = "0" + this.seconds;
+                this.$refs.minutes.placeholder = "00";
+            }
+            if(this.time.length == 0){
+                this.hours = null;
+                this.minutes = null;
+                this.seconds = null;
+                this.$refs.minutes.placeholder = "25";
+            }
+        },
+        focus(){
+            this.$refs.input.focus();
+            this.update();
+            this.$refs.seconds.style.borderRight = `solid`;
+            this.$refs.seconds.style.paddingRight = `0px`;
+            this.cursor = setInterval(()=>{
+                this.$refs.seconds.style.borderRight = `solid`;
+                this.$refs.seconds.style.paddingRight = `0px`;
+                setTimeout(()=>{
+                    this.$refs.seconds.style.borderRight = `none`;
+                    this.$refs.seconds.style.paddingRight = `2px`;
+                }, 500)
+                
+            }, 1000)
+
+        },
+        setTime() {
+            let seconds  = this.seconds ? this.seconds : 0;
+            let minutes  = this.minutes ? this.minutes : 0;
+            let hours  = this.hours ? this.hours : 0;
+            let sendTime = parseInt(hours * 3600 + minutes * 60 + seconds * 1);
+            if (sendTime == 0){
+                sendTime = 1500;
+            }
+            console.log("emitting time: " + sendTime);
+            this.$emit('set-time', {time: sendTime});
+            clearInterval(this.cursor)
         }
     }
 }
@@ -44,21 +107,20 @@ let timerGraphic = {
     <div class= "timer-container">
         <div class = "tomato outline" />
         <div class = "tomato cover" v-bind:style="{ height: increment }" />
-        <div class = "tomato fill"" />
+        <div class = "tomato fill" />
     </div>
     `,
     props:['time', 'set_time'],
     computed:{
         increment () {
-            let mult = 1 - this.time / this.set_time
-            console.log(mult);
-            return mult * 175 + 'px'
+            let mult = 1 - this.time / this.set_time;
+            return mult * 175 + 'px';
         }
     }
 }
 
-let tomatoTimer = new Vue({
-    el: "#tomatoTimer",
+let app = new Vue({
+    el: "#app",
     components:{
         "timer-graphic" : timerGraphic,
         "timer-set" : timerSet
@@ -83,8 +145,8 @@ let tomatoTimer = new Vue({
     },
     data:{
         running: false,
-        set_time: 10,
-        time: 10,
+        set_time: 1500,
+        time: 1500,
         timer: null,
     },
     methods: {
@@ -108,6 +170,10 @@ let tomatoTimer = new Vue({
         stop(){
             this.pause();
             this.time = this.set_time;
+        },
+        set(emit){
+            this.set_time = emit.time;
+            this.time = emit.time;
         }
     }
 });
